76fd56f2b85622c28b25f5cf2a71658f5a70141f
Karikolraj G <ggkarikolraj_bcs28@mepcoeng.ac.in>
2026-08-03 11:06:47 +0530

    Add PrimeFib.c to print prime and Fibonacci numbers using forks

---
+++ exercise1/PrimeFib.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<unistd.h>
+void printprime(int n) {
+   printf("p2 First %d prime numbers:\t",n);
+   int count =0,num=2;
+   while (count<n) {
+      int i,isPrime =1;
+      for (i=2;i*i<=num;i++) {
+	 if(num%i==0) {
+	    isPrime =0;
+	    break;}
+      }
+      if (isPrime) {
+	 printf("%d\t",num);
+	 count++;
+      }
+      num++;
+   }
+   printf("\n");
+}
+void printfibo (int n) {
+   printf("P3 First %d Fibonacci Numbers:\t",n);
+   long a=0,b=1;
+   int i;
+   for (i=0;i<n;i++) {
+      printf("%d\t",a);
+      long next = a+b;
+      a=b;
+      b=next;
+   }
+   printf("\n");
+}
+void main() {
+   int p2,n;
+   printf("Enter n value:");
+   scanf("%d",&n);
+   p2 = fork();
+   if(p2==0)
+      printprime(n);
+   else {
+      int p3;
+      p3 = fork();
+      if (p3==0)
+	 printfibo(n);
+   }
+}
